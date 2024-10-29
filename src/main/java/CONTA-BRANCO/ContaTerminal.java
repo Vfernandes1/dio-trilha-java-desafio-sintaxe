675cb5b05589c5a0c63b11bc5bc9eaf179d03d86
Vfernandes1 <vfernandes1>
2024-10-28 23:42:31 -0300

    feat() - projeto dio entrega

---
+++ src/main/java/CONTA-BRANCO/ContaTerminal.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws exception {
+
+        System.out.println("Por favor, digite o seu nome ");
+        String scanNomeCliente = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        int scanNumero = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String scanAgencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da Agência !");
+        double scanSaldo = scanner.nextDouble();
+
+        System.out.println(("Olá " +  scanNomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + scanAgencia + , " +
+                "conta " + scanNumero + " e seu saldo " + scanSaldo + "já está disponível para saque");
+    }
+}
